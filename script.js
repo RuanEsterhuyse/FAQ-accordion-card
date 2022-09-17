5cbe75d815af9e276b34cef4cc5023b3ab79c716
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-09-17 10:50:28 -0400

    add JS

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
-    const questions = document.querySelectorAll(".question")
-    questions.forEach((question) => question.addEventListener("click", () =>{
-  
-        if(question.parentNode.classList.contains("active")){
-            question.parentNode.classList.toggle("active")
-        }
-    else{
-    questions.forEach(question => question.parentNode.classList.remove("active"))
-    question.parentNode.classList.add("active")
-        }
-
-    }))
+    
+    window.addEventListener("DOMContentLoaded", ()=>{
+        const questions = document.querySelectorAll(".question");
+        questions.forEach((question) => question.addEventListener("click", () =>{
+            if(question.parentNode.classList.contains("active")){
+                question.parentNode.classList.toggle("active")
+            }
+        else{
+        questions.forEach(question => question.parentNode.classList.remove("active"))
+        question.parentNode.classList.add("active")
+            }
+        }));
+    });
+    
+ 
 
 
 
